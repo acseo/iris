cbd718818d8b2597b3a1d8db54a5d8614d4bfa55
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2022-06-24 14:27:18 +0200

    add new route

--- server.js
+++ server.js
@@ -198,6 +198,80 @@ app.get('/iris', (req, res) => {
   res.sendStatus(404)
 })
 
+app.get('/irises', (req, res) => {
+  if (!req.query.coordinates || !req.query.codeCommune) {
+    return res.sendStatus(400)
+  }
+
+  const coordinates = JSON.parse(req.query.coordinates);
+
+  let jsonCityFile = path.resolve(`./dist/`, 'iris-'+req.query.codeCommune+'.json');
+
+  if (!fs.existsSync(jsonCityFile)) {
+    features = grepWithShell(`./dist/iris.json`, "\"codeCommune\":\"".concat(req.query.codeCommune).concat("\""))
+    fs.writeFileSync(jsonCityFile, JSON.stringify(features));  
+  } else {
+    features = JSON.parse(fs.readFileSync(jsonCityFile, 'utf8'));
+  }
+
+  const geoIndex = new Flatbush(features.length)
+  features.forEach(f => geoIndex.add(...getBbox(f)))
+  geoIndex.finish()
+   
+  let result = new Array();
+  for (coordinate of coordinates) {
+    let lat = Number.parseFloat(coordinate.lat);
+    let lon = Number.parseFloat(coordinate.lon);
+    if (lat > 90 || lat < -90 || lon > 180 || lon < -180) {
+      continue;
+    }
+
+    let candidates = geoIndex.neighbors(lon, lat, 10, 10, undefined)
+    .filter(i => features[i].properties.codeCommune === req.query.codeCommune)
+    .map(i => features[i])
+
+    if (candidates.length === 0) {
+      continue;
+    }
+
+    let exactResult = candidates.find(c => {
+      return booleanPointInPolygon(
+        point([lon, lat]),
+        c
+      )
+    })
+  
+    if (exactResult) {
+      result.push(exactResult);
+      continue;
+    }
+
+    let fuzzyResult = chain(candidates)
+      .minBy(c => {
+        const rings = []
+
+        if (c.geometry.type === 'Polygon') {
+          rings.push(...c.geometry.coordinates)
+        }
+
+        if (c.geometry.type === 'MultiPolygon') {
+          c.geometry.coordinates.forEach(polygonRings => rings.push(...polygonRings))
+        }
+
+        return min(rings.map(ring => {
+          return pointToLineDistance(point([lon, lat]), lineString(ring), {units: 'kilometers'})
+        }))
+      })
+      .value()
+
+    if (fuzzyResult) {
+      result.push(fuzzyResult);
+    }
+  }
+
+  res.send(result);
+})
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
